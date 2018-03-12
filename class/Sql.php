4b811fc589816c29913f0e3eea0be0aba985cc1a
Nickolas <nickolasvacarenga@gmail.com>
2018-03-12 16:19:18 -0300

    Criamos a classe usuario

--- class/Sql.php
+++ class/Sql.php
@@ -1,49 +1,28 @@
 <?php 
-
 class Sql extends PDO {
-
 	private $conn;
-
 	public function __construct(){
-
-		$this->conn = new PDO("mysql:host:localhost,dbname:dbphp7", "root", "");
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
-
-	private function setParams($statmente, $parameters = array()){
-
-			foreach ($parameters as $key => $value) {
+	private function setParams($statement, $parameters = array()){
+		foreach ($parameters as $key => $value) {
 			
-			$stmt->bindParam($key, $value);
-
+			$this->setParam($statement, $key, $value);
 		}
 	}
-
-	private function setParam($statment, $key, $value){
-
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
-
-
 	public function query($rawQuery, $params = array()){
-
 		$stmt = $this->conn->prepare($rawQuery);
-
 		$this->setParams($stmt, $params);
-
 		$stmt->execute();
-
 		return $stmt;
-
 	}
-
-	public function select ($rawQuery, $params = array()):array{
-
+	public function select($rawQuery, $params = array()):array
+	{
 		$stmt = $this->query($rawQuery, $params);
-
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 	}
-
 }
-
  ?>
